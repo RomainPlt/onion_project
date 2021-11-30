5126a596398a5d0b613db6ef3be7730c1bafb9fc
romainplt <romain.pialat@imt-atlantique.net>
2021-11-30 20:32:44 +0100

    Added single step decryption

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from flask.helpers import send_from_directory
 import os
 import requests
 import json 
+import aesgcm
 import base64
 
 server = Flask(__name__)
 DIRECTORY= os.getcwd()
 
-certif = DIRECTORY + "/conf/client.crt"
-key = DIRECTORY + "/conf/client-key.key"
+my_server_key = "romainpialatmaisimaginecest256bitslol"
+
 
 @server.route("/files/<path:path>")
 def get_file(path):
@@ -19,6 +20,8 @@ def get_file(path):
 @server.route("/", methods=["POST"])
 def server_sollicitation():  
     data = request.get_json()
+    data = decrypt_data(data)
+    print(data)
     to_server = data["to_server"]
     if to_server:
         addresses = data["send_addr"]
@@ -32,6 +35,26 @@ def server_sollicitation():
         data["return_addr"] = addr_list
         forward(next_address, method, data)
         return "", 201
+
+def decrypt_data(data):
+    decrypted_data = {}
+    for i in data:
+        decrypted_tuple = {}
+        if i != 'to_server':
+            for j in data[i]:
+                dec_key = decrypt_message(j)
+                dec_value = decrypt_message(data[i][j])
+                decrypted_tuple[dec_key] = dec_value
+        elif i == 'to_server':
+            decrypted_tuple[i] = data[i]
+        decrypted_data[i] = decrypted_tuple
+    return decrypted_data
+
+def decrypt_message(ciphertext):
+    key = my_server_key
+    ciphertext = eval(ciphertext)
+    res = aesgcm.main(ciphertext, key, False)
+    return res.decode()
 
 def get_next_address(addr_list):
     next_addr = list(addr_list.keys())[0]
