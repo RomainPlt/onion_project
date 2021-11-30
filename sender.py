0bbf23d8d710c89fdad2d0d3fc4865dd3179b9f7
romainplt <romain.pialat@imt-atlantique.net>
2021-11-30 09:34:37 +0100

    sender and server one way working
    Sender sends a list of nodes, a sending address and a final address
    Servers forward that list minus the address to which they send it
    And replacing the sending address by their own

--- sender.py
+++ sender.py
@@ -3,19 +3,53 @@
 # Send message
 
 import requests
+from flask import Flask, request
+
+my_url = "http://localhost:8080/"
 
 url1 = "http://localhost:8081/"
 url2 = "http://localhost:8082/"
 url3 = "http://localhost:8083/"
-url4 = "http://localhost:8084/toto.txt"
+url4 = "http://localhost:8084/"
+final_url = "http://localhost:8085/files/toto.txt"
+
+list_of_urls = [url2,url3,url4]
+
+data = {"nodes_urls": list_of_urls, 
+        "final_url": final_url,
+        "sending_address": my_url}
+
+def create_address_list(number_nodes):
+    address_list = []
+    for i in range(number_nodes):
+        address_list.append("http://localhost:808"+str(i+1))
+    return address_list
+
+def create_dic_with_protocol(address_list):
+    protocols = {}
+    for i in address_list:
+        protocols[i] = "post"
+    return protocols 
 
 
-list_of_urls = url2+";"+url3+";"+url4+";"
 
 def get_stuff(url):
     print(list_of_urls)
-    r = requests.post(url= url, data=list_of_urls)
+    r = requests.post(url, json=data, )
     print(r.content.decode())
+
+
+client = Flask(__name__)
+
+@client.route("/", methods=["POST"])
+def get_response():
+    data = request.get_json()
+    print(data["response"])
 
 if __name__ == "__main__":
     get_stuff(url1)
+    client.run(
+            host="0.0.0.0",
+            port=8080,
+            threaded=True
+        )
--- server.py
+++ server.py
@@ -1,43 +1,65 @@
-# Should receive a message
-# Decrypt it
-# Get if it is the last hop or not
-# If not last hop
-    # Send to the next server
-# Else
-    # HTTP request to resolve
-
-
 from flask import Flask, request
 from flask.helpers import send_from_directory
 import os
+import requests
 
 server = Flask(__name__)
 UPLOAD_DIRECTORY= os.getcwd()
 
+class Server:
+    def __init__(self, precendent_address, next_address, my_addr):
+        self.prec_addr = precendent_address
+        self.next_addr = next_address
+        self.addr = my_addr
 
-@server.route("/files/<path:path>")
+    def send_packet(self, next_address, other_nodes_address, my_addr, final_addr):
+        data = {"nodes_urls": other_nodes_address,
+                "final_url": final_addr,
+                "sending_address": my_addr}
+        print("Sending ", data, " to ", next_address)
+        r = requests.post(next_address, json=data)
+        return r.content.decode()
+
+
+@server.route("/files/<path:path>") # send back toto.txt
 def get_file(path):
     return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
+
 @server.route("/", methods=["POST"])
-def keep_going_or_not():  
-    data = request.get_data().decode()
-    if data == "":
-        print("No more addresses : I AM THE ONLY ONE")
+def server_sollicitation():  
+    data = request.get_json()
+    nodes = data["nodes_urls"]
+    precedent = data["sending_address"]
+    final = data["final_url"]    
+    if nodes == []:
+        print("Out of Tor network")
+        r = requests.get(final)
+        print(r.content.decode())
     else :
-        next_address = get_next_address(data)
-        forward(next_address)
+        print("Forwarding ...")
+        next_address, address_list = get_next_address(nodes)
+        my_server = Server(precedent, next_address, my_address)
+        my_server.send_packet(next_address, address_list, my_address, final)
     return "", 201
 
-def forward(address):
-    return 0
+def forward(next_adress, other_nodes_address, final_address):
+    data = {"nodes_urls": other_nodes_address,
+            "final_url": final_address}
+    print("Sending ", data, " to ", next_adress)
+    r = requests.post(next_adress, json=data)
+    return r.content.decode()
 
 def get_next_address(addresses_list):
-    return 0
+    next_addr = addresses_list[0]
+    other_nodes = addresses_list[1:]
+    return next_addr, other_nodes
 
 if __name__ == "__main__":
+    port = input("input port number : \n")
+    my_address = "http://localhost:" + port
     server.run(
             host="0.0.0.0",
-            port=8081,
+            port=port,
             threaded=True
         )
