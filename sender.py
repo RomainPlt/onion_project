e84cf305093e526cd60b8404c1f7ca5622bcbb1f
romainplt <romain.pialat@imt-atlantique.net>
2021-11-30 20:32:25 +0100

    Added single step encryption

--- sender.py
+++ sender.py
@@ -1,7 +1,3 @@
-# Should encrypt n times a message
-# Exchange keys with the n different servers
-# Send message
-
 import requests
 import os
 import aesgcm
@@ -17,25 +13,27 @@ def create_address_list(number_nodes):
     address_list = []
     return_addresses = [""]*number_nodes
     for i in range(number_nodes):
-        address_list.append(encrypt_message(("http://localhost:808"+str(i+1)), my_key))
-        return_addresses[i] = encrypt_message(("http://localhost:808"+str(number_nodes-i-1)), my_key)
+        address_list.append(encrypt_message("http://localhost:808"+str(i+1)))
+        return_addresses[i] = encrypt_message("http://localhost:808"+str(number_nodes-i-1))
     return address_list, return_addresses
 
 def create_dic_with_protocol(address_list):
     protocols = {}
     for i in address_list:
-        protocols[i] = encrypt_message("post", my_key)
+        protocols[i] = encrypt_message("post")
     return protocols 
 
 def send_packet(url, data):
-    requests.post(url, data=data)
+    requests.post(url, json=data)
 
-def encrypt_message(message, key):
+def encrypt_message(message):
+    key = my_key
     ciphertext = aesgcm.main(message, key, True)
-    return ciphertext
+    return str(ciphertext)
 
-
-def decrypt_message(ciphertext, key):
+def decrypt_message(ciphertext):
+    key = my_key
+    ciphertext = eval(ciphertext)
     res = aesgcm.main(ciphertext, key, False)
     return res.decode()
 
@@ -43,14 +41,14 @@ def main():
     send_address, return_addr = create_address_list(number_node)
     send_addr_dic = create_dic_with_protocol(send_address)
     return_addr_dic = create_dic_with_protocol(return_addr)
-    send_addr_dic[encrypt_message(target_url, my_key)] = encrypt_message("get", my_key)
-    return_addr_dic[my_url] = encrypt_message("post", my_key)
+    send_addr_dic[encrypt_message(target_url)] = encrypt_message("get")
+    return_addr_dic[encrypt_message(my_url)] = encrypt_message("post")
     next_addr = list(send_addr_dic.keys())[0]
     send_addr_dic.pop(next_addr)
     data = {"send_addr" : send_addr_dic,
             "return_addr" : return_addr_dic,
             "to_server": True}
-    next_addr = decrypt_message(next_addr, my_key)
+    next_addr = decrypt_message(next_addr)
     print(next_addr)
     send_packet(next_addr, data)
 
