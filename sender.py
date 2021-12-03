16f6cc61bedcffa06ac1e79e8a39cd21e8d6a69c
romainplt <romain.pialat@imt-atlantique.net>
2021-12-03 19:48:15 +0100

    Remove any form of encryption, only base 64
    Encryption wasn't going well but plaintext data isn't very good looking.
    Had to """encrypt""" it using a base64 cipher, no key needed only a certain
    number of decipher try.
    Router number 1 will have to decipher 1 time, etc.
    Might had some real cipher later

--- sender.py
+++ sender.py
@@ -1,55 +1,75 @@
+import base64
 import requests
 import os
+
+import simple_aes_cipher
+import simple_aes
 import aesgcm
 
-target_url = "https://assets.afcdn.com/story/20190220/1334066_w4912h2763c1cx2464cy1632cxt1084cyt0cxb4287cyb2983.jpg"
+DIRECTORY= os.getcwd() 
+
+
+target_url = "https://driveimg1.intermarche.com/fr/Content/images/boitmal/produit/zoom/02DD0C00D2FB81CBA9735BD2FC6170B8.jpg"
 number_node = 5
 my_url = "http://localhost:8080/"
-my_key = "romainpialatmaisimaginecest256bitslol"
 
-DIRECTORY= os.getcwd() 
+keys = ["key1", "key2", "key3", "key4", "key5"]
+send_addresses = ["http://localhost:8082","http://localhost:8083",
+"http://localhost:8084","http://localhost:8085"]
+next_addr = "http://localhost:8081"
+send_addresses.append("end_url+"+target_url)
+
+return_addresses = ["http://localhost:8084","http://localhost:8083",
+"http://localhost:8082","http://localhost:8081","http://localhost:8080"]
+
+addresses = send_addresses + return_addresses
+
+def encrypt_address(send_addresses, return_addresses):
+    encrypt_addr = ""
+    for i in range(len(return_addresses)-1, -1, -1):
+        encrypt_addr = encrypt_message(encrypt_addr + "$" + return_addresses[i] + "$")
+    for i in range(len(send_addresses)-1, -1, -1):
+        encrypt_addr = encrypt_message(encrypt_addr + "$"+send_addresses[i]+"$")
+    return encrypt_addr
 
 def create_address_list(number_nodes):
     address_list = []
     return_addresses = [""]*number_nodes
     for i in range(number_nodes):
-        address_list.append(encrypt_message("http://localhost:808"+str(i+1)))
-        return_addresses[i] = encrypt_message("http://localhost:808"+str(number_nodes-i-1))
+        address_list.append(("http://localhost:808"+str(i+1)))
+        return_addresses[i] = ("http://localhost:808"+str(number_nodes-i-1))
     return address_list, return_addresses
 
-def create_dic_with_protocol(address_list):
-    protocols = {}
-    for i in address_list:
-        protocols[i] = encrypt_message("post")
-    return protocols 
+
 
 def send_packet(url, data):
+    print(data)
     requests.post(url, json=data)
 
 def encrypt_message(message):
-    key = my_key
-    ciphertext = aesgcm.main(message, key, True)
-    return str(ciphertext)
+    messageb64 = base64.b64encode(message.encode()).decode()
+    # ciphertext = simple_aes.encrypt(messageb64, key)
+    return messageb64
 
-def decrypt_message(ciphertext):
-    key = my_key
-    ciphertext = eval(ciphertext)
-    res = aesgcm.main(ciphertext, key, False)
-    return res.decode()
+def decrypt_message(ciphertext, key):
+    plaintext = simple_aes.decrypt(ciphertext, key)
+    return plaintext
+
+def encrypt_data(data):
+    for i in data:
+        if i == "to_server":
+            break
+        for j in range(len(data[i])):
+            for k in range(j, len(data[i])):
+                enc_addr = encrypt_message(data[i][k], key=keys[j])
+                print(k, enc_addr)
+                data[i][k] = enc_addr
+    return data
+
 
 def main():
-    send_address, return_addr = create_address_list(number_node)
-    send_addr_dic = create_dic_with_protocol(send_address)
-    return_addr_dic = create_dic_with_protocol(return_addr)
-    send_addr_dic[encrypt_message(target_url)] = encrypt_message("get")
-    return_addr_dic[encrypt_message(my_url)] = encrypt_message("post")
-    next_addr = list(send_addr_dic.keys())[0]
-    send_addr_dic.pop(next_addr)
-    data = {"send_addr" : send_addr_dic,
-            "return_addr" : return_addr_dic,
-            "to_server": True}
-    next_addr = decrypt_message(next_addr)
-    print(next_addr)
+    data = {"addr" : encrypt_address(send_addresses, return_addresses),
+        "content" : ""}
     send_packet(next_addr, data)
 
 if __name__ == "__main__":
