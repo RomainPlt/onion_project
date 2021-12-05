6512dd5f73db28769e91fa22f942c3c1864cd47c
romainplt <romain.pialat@imt-atlantique.net>
2021-12-05 16:50:34 +0100

    Retrieve size from packet

---
+++ compare_data.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+def retrieve_data_from_wireshark(file):
+    wjon = pd.read_json(file)
+    shark = pd.DataFrame(wjon["_source"])
+    data = dict(shark["_source"])
+    return data
+
+def check_http(frame):
+    protocols = frame["layers"]["frame"]["frame.protocols"]
+    if 'http' in protocols:
+        return True
+    else: return False 
+
+def keep_http(data):
+    http_data = {}
+    for i in data:
+        if check_http(data[i]):
+            http_data[i] = data[i]
+    return http_data
+
+def clean_http(http):
+    http_uselfull_data = {}
+    for i in http:
+        if list(http[i]['layers']['http'].keys())[0] == "HTTP/1.0 201 CREATED\\r\\n":
+            pass 
+        else :
+            http_uselfull_data[i] = http[i]
+    return http_uselfull_data
+        
+def drop_keys(http):
+    key_to_remove = ["tcp.segments", "tcp", "json", "eth"]
+    for i in http:
+        for j in key_to_remove:
+            http[i]["layers"].pop(j)
+    return http
+
+def print_http_keys(http):
+    for i in http:
+        print(http[i]["layers"].keys())
+
+def get_frame_len(http):
+    length = http["layers"]["http"]["http.content_length_header"]
+    return (int(length))
+
+
+def main():
+    data = retrieve_data_from_wireshark("wireshark.json")
+    http = keep_http(data)
+    http = clean_http(http)
+    http = drop_keys(http)
+    for i in http:
+        print(get_frame_len(http[i]))
+
+if __name__ == "__main__":
+    main()
